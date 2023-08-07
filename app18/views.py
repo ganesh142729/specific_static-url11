abc758275c045aa7a02ccfc53efc743143b8462a
unknown <yempalaku27@gmail.com>
2023-08-07 22:42:36 +0530

    first commit

---
+++ app18/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+
+# Create your views here.
+def first(request):
+    return render(request,'first.html')
+
+def second(request):
+    return render(request,'second.html')
+
+def thred(request):
+    return render(request,'thred.html')
+
+def forth(request):
+    return render(request,'forth.html')
+def five(request):
+    return render(request,'five.html') 
+def six (request):
+    return render(request,'six.html')
+def seven(request):
+    return render(request,'seven.html')
+def eight(request):
+    return render (request,'eight.html')
+def nine(request):
+    return render(request,'nine.html')
+def ten (request):
+    return render(request,'ten.html')
+def leven(request):
+    return render(request,'leven.html')    
